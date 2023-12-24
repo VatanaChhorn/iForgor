e46836bb84f1700c99bee0cd42d7ef600c921d33
vatana chhorn <47716174+VatanaChhorn@users.noreply.github.com>
2023-12-24 17:04:39 +0700

    Initial Commit

---
+++ iForgor/iForgorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iForgorApp.swift
+//  iForgor
+//
+//  Created by Vatana Chhorn on 24/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct iForgorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
